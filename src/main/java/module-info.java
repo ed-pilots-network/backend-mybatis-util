dc484c8c0102ad185b5a1aa618495e2e999a6332
Pieter Van Eeckhout <wn5987@vandevelde.eu>
2023-06-27 12:21:14 +0200

    # making module names uniform

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,6 +1,7 @@
-module io.edpn.backend.mybatis.util {
+module edpn.backend.mybatis.util {
     requires java.sql;
     requires org.mybatis.spring;
     requires org.mybatis;
+
     exports io.edpn.backend.mybatisutil;
 }
